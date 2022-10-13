e18b775ba055a16e2a4e2cdfd5e81607957a1dfc
Marwan Attar <marwan.attar@ibm.com>
2022-10-14 09:57:52 +1100

    Added prereqs and cpu load simulator

---
+++ cpu-test/cpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true
+do
+  # curl -XGET http://payment-route-cp4i.itzroks-666000qmn3-lxe97o-6ccd7f378ae819553d37d5f2ee142bd6-0000.jp-tok.containers.appdomain.cloud/welcome
+  curl -s -o /dev/null -XGET http://payment-service.cp4i.svc.cluster.local:8080/welcome 
+  sleep 0.01
+done
